b286e75b1d9a86cce53b2c961442e867caa85711
Océane <Ocane@172.16.14.152>
2017-12-11 10:21:42 +0100

    test 15654156

--- Sokoban/src/Sokoban.java
+++ Sokoban/src/Sokoban.java
@@ -4,7 +4,7 @@ public class Sokoban {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Test 123");
-		System.out.println("TEst");
+		System.out.println("TEst2");
 	}
 
 }
